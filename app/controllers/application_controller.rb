155510115b5c4a133c9055f9156c2e62f7292a37
tangbt <tangbt_qzy@126.com>
2014-12-26 10:29:24 +0800

    change resource and added message info for commit info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+ # used for display message in application.html.erb
+  def bootstrap_class_for flash_type
+    case flash_type
+    when "success"
+      "alert-success" # Green
+    when "error"
+      "alert-danger" # Red
+    when "alert"
+      "alert-warning" # Yellow
+    when "notice"
+      "alert-info" # Blue
+    else
+      flash_type.to_s
+    end
+  end
 end
